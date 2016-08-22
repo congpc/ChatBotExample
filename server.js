5ad944f082113a591ad7e201a94fd95a290fb219
Pham Chi Cong <congpc@MacBook-Pro-cua-Pham.local>
2016-08-22 22:44:19 +0700

    Modify ip and log

--- server.js
+++ server.js
@@ -258,7 +258,7 @@ router.post('/webhook', function (req, res) {
   }
 });
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 4000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
